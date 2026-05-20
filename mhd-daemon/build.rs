10f225663e4d15c7f98cefed17d18e14751e7619
Artur <arthurka.x.r@gmail.com>
2026-05-20 17:00:26 +0400

    fix: add cargo:rerun-if-changed for icon files in build.rs

--- mhd-daemon/build.rs
+++ mhd-daemon/build.rs
@@ -1,3 +1,8 @@
 fn main() {
     let _ = embed_resource::compile("../icons/mhd.rc", embed_resource::NONE);
+
+    // Ensure cargo re-runs the build script when the icon source changes
+    println!("cargo:rerun-if-changed=../icons/mHD_256.png");
+    println!("cargo:rerun-if-changed=../icons/mhd.ico");
+    println!("cargo:rerun-if-changed=../icons/mhd.rc");
 }
